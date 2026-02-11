0f2e410c2ff1d3019ac0a3c99f204877c7b4b614
darkgreyb <mahashnaigat@yahoo.com>
2026-02-12 00:13:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     
-    valentineName: "Lalida",
+    valentineName: "Hakkı",
 
     // The title that appears in the browser tab
     // You can use emojis!
-    pageTitle: "Will You Be My Valentine, Schatz? 🩵",
+    pageTitle: "Will You Be My Valentine, canım? 🩵",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
@@ -23,19 +23,14 @@ const CONFIG = {
     questions: {
         first: {
             text: " Do you like me ?",                                    // First interaction
-            yesBtn: "Ja",                                             // Text for "Yes" button
-            noBtn: "Nein",                                               // Text for "No" button
+            yesBtn: "Yes",                                             // Text for "Yes" button
+            noBtn: "No",                                               // Text for "No" button
             secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
         },
         second: {
-            text: "How much do you love me?",                          // For the love meter
-            startText: "This much!",                                   // Text before the percentage
-            nextBtn: "Next ❤️"                                         // Text for the next button
-        },
-        third: {
-            text: "Will you be my Valentine on February 14th, 2026? 🌹", // The big question!
-            yesBtn: "JAAAAA !",                                             // Text for "Yes" button
-            noBtn: "NEIN🥹"                                                 // Text for "No" button
+            text: "Will you be my Valentine🌹", // The big question!
+            yesBtn: "YES !",                                             // Text for "Yes" button
+            noBtn: "NO🥹"                                                 // Text for "No" button
         }
     },
 
@@ -49,8 +44,8 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 😭🩵🩵🩵",
-        message: "Now come get your gift, a big warm hug and a huge kiss😘🐥🩵!",
+        title: "Yay! I'm the luckiest person in the world! 😭🩵",
+        message: "Kiss you baby😘!",
         emojis: "💖💖💖"  // These will bounce around
     },
 
